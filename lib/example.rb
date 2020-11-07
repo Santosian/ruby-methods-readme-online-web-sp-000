a6340afde40fa6d2d7f439889343d17b81686295
Santiago Molinari <santomolinari@gmail.com>
2020-11-07 18:04:32 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,10 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def  say_hello_ten_times
+  phrase = "Hello World!"
+  10.times do
+    puts phrase
+  end
+end
+
+say_hello_ten_times
